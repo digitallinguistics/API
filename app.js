4e59779eafdba55aabffc9e685ed006b34804109
Daniel W. Hieber <dwhieb@gmail.com>
2016-01-14 01:38:17 -0800

    adds initial user registration

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(middleware.logUrl); // url logging for debugging
 app.use(express.static(__dirname + '/public')); // routing for static files
 app.use(bodyParser.urlencoded({ extended: false })); // parse form data
 app.use(cookieParser(credentials.secret)); // cookie handling
+app.use(middleware.userInfo); // checks for user info in cookies and adds to locals if present
 
 // routing
 require('./lib/router')(app);
