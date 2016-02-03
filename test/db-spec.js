f327147f3243ff946e3125f2b040b5bfc1ed527e
Daniel W. Hieber <dwhieb@gmail.com>
2016-02-02 22:21:34 -0800

    passing test
    database API can create users with email IDs
    
    Also adds afterAll to delete any leftover items from the database

--- test/db-spec.js
+++ test/db-spec.js
@@ -7,12 +7,33 @@ describe('the database API', function () {
     this.db = require('../lib/db');
     this.collection = 'texts';
 
+    this.users = [
+      { id: 'me@example.com', firstName: 'John', lastName: 'Doe' },
+      { id: 'me@test.com', firstName: 'Jane', lastName: 'Doe' }
+    ];
+
     this.results = [];
 
     this.db.ready().then(() => {
       console.log('Database ready.');
       done();
-    });
+    }).catch(err => console.error(err));
+
+  });
+
+  afterAll(function (done) {
+
+    const rids = this.users.map(user => user._rid);
+
+    const deleteUsers = this.db.delete('users', rids)
+    .then(res => {
+      if (res.every(response => response.status === 204)) { console.log('Users deleted.'); }
+      else { console.error('Problem deleting users.'); }
+    }).catch(err => console.error('Error in afterAll:', err));
+
+    // const deleteDocs = this.db.delete('lexEntries', this.results.map(lexEntry => lexEntry._rid));
+
+    Promise.all([deleteUsers]).then(done).catch(err => console.error(err));
 
   });
 
@@ -35,10 +56,7 @@ describe('the database API', function () {
       expect(Number.isInteger(+res.id)).toBe(true);
       this.results.push(res);
       done();
-    }).catch(err => {
-      console.error(err);
-      done();
-    });
+    }).catch(err => { fail(err); done(); });
 
   });
 
@@ -72,11 +90,7 @@ describe('the database API', function () {
       expect(res[0].id).toMatch(/^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$/i);
       this.results.push(...res);
       done();
-    }).catch(err => {
-      console.error(err);
-      done();
-    });
-
+    }).catch(err => { fail(err); });
   });
 
   it('can get a document', function (done) {
@@ -84,11 +98,7 @@ describe('the database API', function () {
     .then(text => {
       expect(text).toEqual(this.results[1]);
       done();
-    }).catch(err => {
-      console.error(err);
-      done();
-    });
-  });
+    }).catch(err => { fail(err); }); });
 
   it('can get multiple documents', function (done) {
     const rids = this.results.slice(1).map(text => text._rid);
@@ -99,22 +109,14 @@ describe('the database API', function () {
       expect(rids.includes(res[0]._rid)).toBe(true);
       expect(rids.includes(res[1]._rid)).toBe(true);
       done();
-    }).catch(err => {
-      console.error(err);
-      done();
-    });
-  });
+    }).catch(err => { fail(err); }); });
 
   it('can get a document by ID', function (done) {
     this.db.getById(this.collection, this.results[0].id, { idType: 'id' })
     .then(text => {
       expect(text).toEqual(this.results[0]);
       done();
-    }).catch(err => {
-      console.error(err);
-      done();
-    });
-  });
+    }).catch(err => { fail(err); }); });
 
   it('can get multiple documents by ID', function (done) {
     const ids = this.results.map(text => text.id);
@@ -124,10 +126,7 @@ describe('the database API', function () {
       expect(res.length).toEqual(this.results.length);
       expect(this.results).toContain(res[0]);
       done();
-    }).catch(err => {
-      console.error(err);
-      done();
-    });
+    }).catch(err => { fail(err); });
   });
 
   it('can delete a document', function (done) {
@@ -137,11 +136,7 @@ describe('the database API', function () {
       expect(res.status).toBe(204);
       this.results.splice(2, 1);
       done(0);
-    }).catch(err => {
-      console.error(err);
-      done();
-    });
-
+    }).catch(err => { fail(err); });
   });
 
   it('can delete multiple documents', function (done) {
@@ -152,15 +147,38 @@ describe('the database API', function () {
     .then(res => {
       expect(res instanceof Array).toBe(true);
       expect(res.every(response => response.status === 204)).toBe(true);
+      this.results = [];
       done();
+    }).catch(err => { fail(err); });
+
+  });
+
+  it('can create users with email IDs', function (done) {
+
+    this.db.create('users', this.users)
+    .then(res => {
+      expect(res instanceof Array).toBe(true);
+      expect(res.length).toEqual(2);
+      expect(res.map(user => user.id).includes(this.users[0].id)).toBe(true);
+      expect(res.map(user => user.id).includes(this.users[1].id)).toBe(true);
+      this.users.splice(0);
+      this.users.push(...res);
+      done();
+    }).catch(err => { fail(err); });
+
+  });
+
+  it('returns a 409 error when creating a user whose email already exists', function (done) {
+
+    this.db.create('users', this.users)
+    .then(res => {
+      fail('Error not thrown. Received response:', res);
     }).catch(err => {
-      console.error(err);
+      expect(err.status).toBe(409);
       done();
     });
 
   });
-
-  it('can create users with email IDs');
 
   it('can get a user by service ID');
 
@@ -178,6 +196,13 @@ describe('the database API', function () {
 
   it('can upsert multiple existing documents');
 
-  it('can handle many requests in sequence');
+  xit('can handle throttled requests (many requests in sequence)', function (done) {
+
+    done();
+
+    // const ids = Array(200).fill(null).map((item, i, arr) => arr[i] = i);
+    // push created items to this.results for use in afterAll
+
+  });
 
 });
