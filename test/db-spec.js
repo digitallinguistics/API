94645bac6d7a02068a646d3aaab84c1b409d0638
Daniel W. Hieber <dwhieb@gmail.com>
2016-02-02 18:42:07 -0800

    more tests passing

--- test/db-spec.js
+++ test/db-spec.js
@@ -1,3 +1,5 @@
+require('../lib/utils');
+
 describe('the database API', function () {
 
   beforeAll(function (done) {
@@ -6,16 +8,36 @@ describe('the database API', function () {
 
     this.results = [];
 
-    this.texts = [
-      {
-        title: 'How the world began',
-        phrases: [
-          {
-            transcription: 'waštʼunkʼu ʔasi',
-            translation: 'one day a man'
-          }
-        ]
-      },
+    this.db.ready().then(done);
+
+  });
+
+  it('can create a document', function (done) {
+
+    const text = {
+      title: 'How the world began',
+      phrases: [
+        {
+          transcription: 'waštʼunkʼu ʔasi',
+          translation: 'one day a man'
+        }
+      ]
+    };
+
+    this.db.create('users', text, { createId: true })
+    .then(res => {
+      expect(res instanceof Object).toBe(true);
+      expect(res instanceof Array).toBe(false);
+      expect(Number.isInteger(+res.id)).toBe(true);
+      this.results.push(res);
+      done();
+    }).catch(err => console.error(err));
+
+  });
+
+  it('can create multiple documents', function (done) {
+
+    const texts = [
       {
         title: 'How the Indian came',
         phrases: [
@@ -36,33 +58,36 @@ describe('the database API', function () {
       }
     ];
 
-    this.db.ready().then(done);
-
-  });
-
-  it('can create a document', function (done) {
-    this.db.create('users', this.texts[0], { createId: true })
-    .then(res => {
-      expect(res instanceof Object).toBe(true);
-      expect(res instanceof Array).toBe(false);
-      expect(Number.isInteger(+res.id)).toBe(true);
-      done();
-    }).catch(err => console.error(err));
-  });
-
-  it('can create multiple documents', function (done) {
-    this.db.create('texts', this.texts)
+    this.db.create('texts', texts)
     .then(res => {
       expect(res instanceof Array).toBe(true);
-      expect(res.length).toEqual(3);
+      expect(res.length).toEqual(2);
       expect(res[0].id).toMatch(/^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$/i);
+      this.results.push(...res);
+      done();
+    }).catch(err => console.error(err));
+
+  });
+
+  it('can get a document', function (done) {
+    this.db.get('texts', this.results[1]._rid)
+    .then(text => {
+      expect(text).toEqual(this.results[1]);
       done();
     }).catch(err => console.error(err));
   });
 
-  it('can get a document');
-
-  it('can get multiple documents');
+  it('can get multiple documents', function (done) {
+    const rids = this.results.slice(1).map(text => text._rid);
+    this.db.get('texts', rids)
+    .then(res => {
+      expect(res instanceof Array).toBe(true);
+      expect(res.length).toEqual(rids.length);
+      expect(rids.includes(res[0]._rid)).toBe(true);
+      expect(rids.includes(res[1]._rid)).toBe(true);
+      done();
+    }).catch(err => console.error(err));
+  });
 
   it('can get a document by ID');
 
