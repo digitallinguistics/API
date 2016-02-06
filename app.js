c9f6e8c3980534082aa444621f2825561ecc4b46
Daniel W. Hieber <dwhieb@gmail.com>
2016-02-06 15:20:27 -0800

    debut 1

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // node modules
 require('./lib/utils');
+const bodyParser = require('body-parser');
+const config = require('./lib/config');
 const http = require('http');
 
 http.createServer(function (req, res) {
-  console.log(req.url);
+  console.log('Debug 1');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello world!');
 }).listen(process.env.PORT);
 
-console.log('Server started.');
+console.log('Server started. Debug 1.');
