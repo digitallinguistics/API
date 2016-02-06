47f72102d643b6c8425a058a0ad609606f959543
Daniel W. Hieber <dwhieb@gmail.com>
2016-02-06 15:25:08 -0800

    testing without credentials

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ require('./lib/utils');
 const bodyParser = require('body-parser');
 const config = require('./lib/config');
 const cookieParser = require('cookie-parser');
-const credentials = require('./lib/credentials');
+// const credentials = require('./lib/credentials');
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Debug 2');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(`<pre>${JSON.stringify(config, null, 2)}</pre>`);
 }).listen(process.env.PORT);
