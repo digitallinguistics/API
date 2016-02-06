ed4732cfa9bc9564e53df862c37915c649ebf4eb
Daniel W. Hieber <dwhieb@gmail.com>
2016-02-06 15:17:54 -0800

    log req.url

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('./lib/utils');
 const http = require('http');
 
 http.createServer(function (req, res) {
+  console.log(req.url);
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello world!');
 }).listen(process.env.PORT);
