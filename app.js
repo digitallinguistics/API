61911ef70ed1a2f08e3b49af493f23a26ecb0b21
Daniel W. Hieber <dwhieb@gmail.com>
2016-02-06 15:22:12 -0800

    debug 2

--- app.js
+++ app.js
@@ -2,12 +2,11 @@
 require('./lib/utils');
 const bodyParser = require('body-parser');
 const config = require('./lib/config');
+const cookieParser = require('cookie-parser');
+const credentials = require('./lib/credentials');
 const http = require('http');
 
 http.createServer(function (req, res) {
-  console.log('Debug 1');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world!');
+  res.end('Debug 2');
 }).listen(process.env.PORT);
-
-console.log('Server started. Debug 1.');
