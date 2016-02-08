0b240022106631004e6255581f0df5dfb63c00c9
Daniel W. Hieber <dwhieb@gmail.com>
2016-02-08 00:52:09 -0800

    passing test
    /oauth returns a 401 response if the sub claim is missing

--- test/oauth-spec.js
+++ test/oauth-spec.js
@@ -68,7 +68,15 @@ describe('/oauth', function () {
     makeRequest(opts, handler);
   });
 
-  it('returns a 401 response if the sub claim is missing');
+  it('returns a 401 response if the sub claim is missing', function (done) {
+    const handler = result => {
+      expect(result.status).toEqual(401);
+      done();
+    };
+    const t = token(null, { subject: undefined });
+    const opts = options({ headers: { Authorization: `Bearer ${t}` } });
+    makeRequest(opts, handler);
+  });
 
   it('returns a 401 response if the service claim is missing');
 
