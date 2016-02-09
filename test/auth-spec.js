93ce5ffb82617dd319a096dd6a7bec4281c18cbb
Daniel W. Hieber <dwhieb@gmail.com>
2016-02-09 01:29:00 -0800

    auth tests passing

--- test/auth-spec.js
+++ test/auth-spec.js
@@ -1,6 +1,7 @@
 const ClientApp = require('../lib/models/client-app');
 const db = require('../lib/db');
 const http = require('http');
+const jwt = require('jsonwebtoken');
 const qs = require('querystring');
 const URL = require('url');
 const User = require('../lib/models/user');
@@ -22,7 +23,7 @@ describe('/auth', function () {
 
   beforeAll(function (done) {
     console.log('Auth: starting');
-    db.upsert('apps', { name: 'Auth Test App' }).then(app => {
+    db.upsert('apps', new ClientApp({ name: 'Auth Test App' })).then(app => {
       this.app = new ClientApp(app);
       this.query = props => {
         const defaults = {
@@ -31,17 +32,32 @@ describe('/auth', function () {
           response_type: 'token',
           state: uuid.v4()
         };
-        Object.assign(defaults, props);
+        Object.assign(defaults, props || {});
         return defaults;
+      };
+      this.token = (payload, opts) => {
+        const defaultPayload = { cid: this.app._rid };
+        const defaultOpts = {
+          algorithm: 'HS256',
+          audience: 'https://api.digitallinguistics.org',
+          expiresIn: 3600,
+          subject: this.user._rid
+        };
+        Object.assign(defaultPayload, payload || {});
+        Object.assign(defaultOpts, opts || {});
+        return jwt.sign(defaultPayload, this.app.secret, defaultOpts);
       };
       this.options = props => {
         const defaults = {
           hostname: 'localhost',
           method: 'GET',
           path: `/auth?${qs.stringify(this.query())}`,
-          port: 3000
+          port: 3000,
+          headers: {
+            Authorization: `Bearer ${this.token()}`
+          }
         };
-        Object.assign(defaults, props);
+        Object.assign(defaults, props || {});
         return defaults;
       };
       return;
@@ -98,12 +114,13 @@ describe('/auth', function () {
     makeRequest(opts, handler);
   });
 
-  // if no token is provided in the Auth or Cookie headers, user is assumed
+  // if no token is provided in the Auth or Cookie headers, user is assumed to not be logged in
   it('redirects to the login page if the user is not logged in', function (done) {
     const q = this.query();
     const state = q.state;
-    const opts = this.options({ path: `/auth?${qs.stringify(q)}` });
+    const opts = this.options({ path: `/auth?${qs.stringify(q)}`, headers: {} });
     const handler = (result, res) => {
+      expect(res.statusCode).toBe(302);
       expect(res.headers.location).toBeDefined();
       const url = URL.parse(res.headers.location);
       expect(url.pathname).toEqual('/login');
@@ -116,5 +133,40 @@ describe('/auth', function () {
     makeRequest(opts, handler);
   });
 
+  it('redirects to the login page if the User ID in the token is invalid', function (done) {
+    const token = this.token({}, { subject: 'badId' });
+    const opts = this.options({ headers: { Authorization: `Bearer ${token}` }});
+    const handler = (result, res) => {
+      const url = URL.parse(res.headers.location);
+      expect(url.pathname).toEqual('/login');
+      expect(url.hostname).toEqual('digitallinguistics.org');
+      done();
+    };
+    makeRequest(opts, handler);
+  });
+
+  it('redirects to the redirect URI if the token is valid', function (done) {
+    const q = this.query();
+    const state = q.state;
+    const opts = this.options({ path: `/auth?${qs.stringify(q)}` });
+    const handler = (result, res) => {
+      expect(res.statusCode).toBe(302);
+      expect(res.headers.location).toBeDefined();
+      const url = URL.parse(res.headers.location);
+      expect(url.hostname).toBe('danielhieber.com');
+      const query = qs.parse(url.query);
+      expect(query.state).toEqual(state);
+      expect(query.access_token).toBeDefined();
+      const opts = {
+        algorithms: ['HS256'],
+        audience: 'https://api.digitallinguistics.org',
+        subject: this.user._rid
+      };
+      const payload = jwt.verify(query.access_token, this.app.secret, opts);
+      expect(payload.cid).toEqual(this.app._rid);
+      done();
+    };
+    makeRequest(opts, handler);
+  });
 
 });
