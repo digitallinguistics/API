7e778f6680314486453bc65a36d31d7ed2fb2ff0
Daniel W. Hieber <dwhieb@gmail.com>
2016-11-30 01:53:24 -0600

    add app.js
    cf. #80

---
+++ app.js
@@ -0,0 +1,33 @@
+const config = require('./lib/config');
+const express = require('express');
+const helmet = require('helmet');
+const http = require('http');
+
+// initialize Express
+const app = express();
+
+// app settings
+app.enable('trust proxy'); // trust the Azure proxy server
+app.set('port', config.port); // set port for the app
+
+// middleware
+app.use(helmet()); // basic security features
+app.use(express.static('swagger')); // routing for static files
+
+// create a server
+const server = http.createServer(app);
+
+// generic error handler
+server.on('error', err => console.error(err, err.stack));
+
+// start the server listening
+server.listen(config.port, () => {
+  console.log(`\nServer started. Press Ctrl+C to terminate.\n
+  Project:  dlx-api
+  Env:      ${config.env}
+  Port:     ${config.port}
+  Node:     ${process.version}
+  Time:     ${new Date}\n`);
+});
+
+module.exports = app;
