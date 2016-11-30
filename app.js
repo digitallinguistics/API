72352f6771e762a533ca5c3acf689e613864360d
Daniel W. Hieber <dwhieb@gmail.com>
2016-11-30 16:16:05 -0600

    error handling
    cf. #80

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const config = require('./lib/config');
 const express = require('express');
 const helmet = require('helmet');
-const http = require('http');
 const middleware = require('./lib/middleware');
 const router = require('./lib/router');
+const server = require('./lib/server');
 
 // initialize Express
 const app = express();
@@ -17,24 +17,8 @@ app.use(helmet()); // basic security features
 app.use(express.static('swagger')); // routing for static files
 app.use(middleware); // custom middleware (logs URL)
 
-// URL routing
-router(app);
-
-// create a server
-const server = http.createServer(app);
-
-// generic error handler
-server.on('error', err => console.error(err, err.stack));
-
-// start the server
-server.listen(config.port, () => {
-  console.log(`\nServer started. Press Ctrl+C to terminate.\n
-  Project:  dlx-api
-  Env:      ${config.env}
-  Port:     ${config.port}
-  Node:     ${process.version}
-  Time:     ${new Date}\n`);
-});
+router(app); // URL routing
+server(app); // create the server
 
 // export app for testing
 module.exports = app;
