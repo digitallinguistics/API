57ba47798ad6e8a79f83cb7e1bae0cc4b86ccee0
Daniel W. Hieber <dwhieb@gmail.com>
2016-11-30 01:59:16 -0600

    add middleware.js
    cf. #80

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require('./lib/config');
 const express = require('express');
 const helmet = require('helmet');
 const http = require('http');
+const middleware = require('./lib/middleware');
 
 // initialize Express
 const app = express();
@@ -13,6 +14,7 @@ app.set('port', config.port); // set port for the app
 // middleware
 app.use(helmet()); // basic security features
 app.use(express.static('swagger')); // routing for static files
+app.use(middleware); // custom middleware (logs URL)
 
 // create a server
 const server = http.createServer(app);
