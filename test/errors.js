72e5cf7980d45c174acdf135450b8d4772bc5344
Daniel W. Hieber <dwhieb@gmail.com>
2016-11-30 23:17:30 -0600

    skeleton specs

--- test/errors.js
+++ test/errors.js
@@ -1,7 +1,65 @@
 /* eslint-disable
+  func-names,
+  max-statements,
   prefer-arrow-callback
 */
 
-describe('errors', function errors() {
+describe('API Errors', function() {
+
+  xit('404: No Route', function(done) {
+
+  });
+
+  xit('405: Method Not Allowed', function(done) {
+
+  });
+
+  xit('497: Use HTTPS', function(done) {
+
+  });
+
+  xit('credentials_required', function(done) {
+
+  });
+
+  xit('invalid_token: aud missing', function(done) {
+
+  });
+
+  xit('invalid_token: aud invalid', function(done) {
+
+  });
+
+  xit('invalid_token: cid missing', function(done) {
+
+  });
+
+  xit('invalid_token: cid invalid', function(done) {
+
+  });
+
+  xit('invalid_token: iss missing', function(done) {
+
+  });
+
+  xit('invalid_token: iss invalid', function(done) {
+
+  });
+
+  xit('invalid_token: scp missing', function(done) {
+
+  });
+
+  xit('invalid_token: scp invalid', function(done) {
+
+  });
+
+  xit('invalid_token: sub invalid', function(done) {
+
+  });
+
+  xit('token_expired', function(done) {
+    // TODO: rename this test?
+  });
 
 });
