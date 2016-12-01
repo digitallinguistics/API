c0243023902e6d70dab57e3802c328f49997fb4b
Daniel W. Hieber <dwhieb@gmail.com>
2016-11-30 21:20:58 -0600

    add body-parser (#100)
    * add body-parser
    
    closes #99
    
    * add Jasmine install to Travis build steps
    
    * rename public folder from 'swagger' > 'public'
    
    * move Swagger files to public/swagger

--- app.js
+++ app.js
@@ -3,26 +3,28 @@ const config = require('./lib/config');
 
 // load dependencies
 const authenticate = require('./lib/authenticate');
+const bodyParser = require('body-parser');
 const express = require('express');
 const helmet = require('helmet');
 const middleware = require('./lib/middleware');
-const router = require('./lib/router');
+const router = require('./lib/routes');
 const server = require('./lib/server');
 
 // initialize Express
 const app = express();
 
 // app settings
-app.enable('trust proxy');          // trust the Azure proxy server
-app.set('port', config.port);       // set port for the app
+app.enable('trust proxy');         // trust the Azure proxy server
+app.set('port', config.port);      // set port for the app
 
 // middleware
-app.use(helmet());                  // basic security features
-app.use(express.static('swagger')); // routing for static files
-app.use(middleware);                // custom middleware (logs URL)
-app.use(authenticate);              // authenticate all requests to the API
+app.use(helmet());                 // basic security features
+app.use(express.static('public')); // routing for static files
+app.use(middleware);               // custom middleware (logs URL)
+app.use(bodyParser.json());        // parse JSON data in the request body
+app.use(authenticate);             // authenticate all requests to the API
 
-router(app);                        // URL routing
-server(app);                        // create the server
+router(app);                       // URL routing
+server(app);                       // create the server
 
-module.exports = app;               // export app for testing
+module.exports = app;              // export app for testing
