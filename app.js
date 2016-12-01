1c34ac7f42df8591b3b6b1363abcbdd46e2b9d89
Daniel W. Hieber <dwhieb@gmail.com>
2016-11-30 21:17:01 -0600

    rename public folder from 'swagger' > 'public'

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ const server = require('./lib/server');
 const app = express();
 
 // app settings
-app.enable('trust proxy');          // trust the Azure proxy server
-app.set('port', config.port);       // set port for the app
+app.enable('trust proxy');         // trust the Azure proxy server
+app.set('port', config.port);      // set port for the app
 
 // middleware
-app.use(helmet());                  // basic security features
-app.use(express.static('swagger')); // routing for static files
-app.use(middleware);                // custom middleware (logs URL)
-app.use(bodyParser.json());         // parse JSON data in the request body
-app.use(authenticate);              // authenticate all requests to the API
+app.use(helmet());                 // basic security features
+app.use(express.static('public')); // routing for static files
+app.use(middleware);               // custom middleware (logs URL)
+app.use(bodyParser.json());        // parse JSON data in the request body
+app.use(authenticate);             // authenticate all requests to the API
 
-router(app);                        // URL routing
-server(app);                        // create the server
+router(app);                       // URL routing
+server(app);                       // create the server
 
-module.exports = app;               // export app for testing
+module.exports = app;              // export app for testing
