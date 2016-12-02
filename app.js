04260fb3ee507ca9af4507d2b155bf8cd6d416ab
Daniel W. Hieber <dwhieb@gmail.com>
2016-12-02 00:27:28 -0600

    add initial code for versioning

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const config = require('./lib/config');
 
 // load dependencies
 const authenticate = require('./lib/authenticate');
-const bodyParser = require('body-parser');
-const express = require('express');
-const helmet = require('helmet');
-const middleware = require('./lib/middleware');
-const router = require('./lib/router');
-const server = require('./lib/server');
+const bodyParser   = require('body-parser');
+const express      = require('express');
+const helmet       = require('helmet');
+const middleware   = require('./lib/middleware');
+const routers      = require('./lib/routers');
+const server       = require('./lib/server');
 
-// initialize Express
-const app = express();
+// initialize Express and routers
+const app = express();             // create the Express app
+const v0 = express.Router();       // create the Version 0.x router
 
 // app settings
 app.enable('trust proxy');         // trust the Azure proxy server
@@ -24,7 +25,13 @@ app.use(middleware);               // custom middleware (logs URL)
 app.use(bodyParser.json());        // parse JSON data in the request body
 app.use(authenticate);             // authenticate all requests to the API
 
-router(app);                       // URL routing
+// add routes
+routers.v0(v0);
+
+// mount routers to their respective API version paths
+app.use(v0);                       // Latest
+app.use('/v0', v0);                // v0.x
+
 server(app);                       // create the server
 
 module.exports = app;              // export app for testing
