2bf251d34f567e5b78d519531b71455cc1fc1a7c
Daniel W. Hieber <dwhieb@gmail.com>
2017-04-16 13:54:46 -0700

    Socket.IO (#117)

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
 // load config file before loading other modules
 const config = require('./lib/config');
 
-// load dependencies
+// load modules
 const authenticate = require('./lib/middleware/authenticate');
 const bodyParser   = require('body-parser');
+const createServer = require('./lib/server');
+const createSocket = require('./lib/socket');
 const error        = require('./lib/middleware/error');
 const express      = require('express');
 const helmet       = require('helmet');
 const logger       = require('./lib/middleware/logger');
-const routers      = require('./lib/routers');
-const server       = require('./lib/server');
+const routers      = require('./lib/routers/rest');
+const sockets      = require('./lib/routers/socket');
 
 // initialize Express and routers
-const app = express();             // create the Express app
-const v0  = express.Router();      // create the Version 0.x router
+const app = express();               // create the Express app
+const v0  = express.Router();        // create the Version 0.x router
 
 // app settings
-app.enable('trust proxy');         // trust the Azure proxy server
-app.set('port', config.port);      // set port for the app
+app.enable('trust proxy');           // trust the Azure proxy server
+app.set('port', config.port);        // set port for the app
 
 // middleware
-app.use(helmet());                 // basic security features
-app.use(bodyParser.json());        // parse JSON data in the request body
-app.use(express.static('public')); // routing for static files
-app.use(error);                    // adds res.error method to response
-app.use(logger);                   // custom middleware (logs URL)
-app.use(authenticate.unless({      // authenticate requests to the API
-  path: [/test/],                  // don't authenticate test routes
+app.use(helmet());                   // basic security features
+app.use(bodyParser.json());          // parse JSON data in the request body
+app.use(express.static('public'));   // routing for static files
+app.use(error);                      // adds res.error method to response
+app.use(logger);                     // custom middleware (logs URL)
+app.use(authenticate.unless({        // authenticate requests to the API
+  path: [/test/],                    // don't authenticate test routes
 }));
 
 // add routes to routers
 routers.v0(v0);
 
 // mount routers to their respective version paths
-app.use(v0);                       // default routing
+app.use(v0);
 app.use('/v0', v0);
 
-routers.common(app);               // add generic routes / error handlers (must be last route)
-server(app);                       // create the server
-module.exports = app;              // export app for testing
+// add test routes and error handlers
+routers.common(app);
+
+const server = createServer(app);    // create the server
+const io     = createSocket(server); // create the socket
+
+// create a socket for each version namespace
+sockets.v0(io);
+sockets.v0(io.router(`/v0`));
+
+module.exports = app;                // export app for testing
