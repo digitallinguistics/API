2abb7fa609b19b6b00c897cfbc0f8793dcc5a2bf
Daniel W. Hieber <dwhieb@gmail.com>
2017-05-03 21:28:36 -0700

    remove socket tests

--- test/index.js
+++ test/index.js
@@ -1,33 +1,16 @@
-/* eslint-disable
-  func-names,
-  max-nested-callbacks,
-  max-statements,
-  newline-per-chained-call,
-  no-magic-numbers,
-  no-shadow,
-  prefer-arrow-callback
-*/
-
-const app          = require(`../app`);
-const req          = require(`supertest`).agent(app);
-const rest         = require(`./rest`);
-const restErrors   = require(`./rest-errors`);
-const socketErrors = require(`./socket-errors`);
+const api    = require(`./api`);
+const app    = require(`../app`);
+const errors = require(`./errors`);
+const req    = require(`supertest`).agent(app);
 
 // test client registration and authentication (with Auth0)
 require(`./authentication`);
 require(`./registration`);
 
 // test errors for for REST API
-restErrors(req);
-restErrors(req, `/v0`);
+errors(req);
+errors(req, `/v0`);
 
 // test endpoints for REST API
-rest(req);
-rest(req, `/v0`);
-
-// test errors for Socket API
-socketErrors();
-socketErrors(`/v0`);
-
-// test events for Socket API
+api(req);
+api(req, `/v0`);
