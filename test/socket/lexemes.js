47025319423dde994b35ddf199d9e48a06530ba5
Daniel W. Hieber <dwhieb@gmail.com>
2017-09-02 15:35:41 -0500

    all tests passing

--- test/socket/lexemes.js
+++ test/socket/lexemes.js
@@ -17,6 +17,7 @@ const { coll, upsert } = db;
 
 const languageID = `12345`;
 const test       = true;
+const type       = `Lexeme`;
 
 const permissions = {
   contributors: [],
@@ -30,7 +31,7 @@ const defaultData = {
   lemma: {},
   permissions,
   test,
-  type: `Lexeme`,
+  type,
 };
 
 const lang = {
@@ -65,7 +66,7 @@ module.exports = (v = ``) => {
 
     }));
 
-    fit(`add`, testAsync(async function() {
+    it(`add`, testAsync(async function() {
 
       // add Lexeme with Language that exists
       const data = Object.assign({ tid: `addLexeme` }, defaultData);
@@ -76,7 +77,7 @@ module.exports = (v = ``) => {
       const lex1 = Object.assign({}, defaultData);
 
       try {
-        const { info } = await emit(`add`, `Lexeme`, { languageID: `678910` }, lex1);
+        const { info } = await emit(`add`, `Lexeme`, lex1, { languageID: `678910` });
         expect(info.status).toBe(404);
       } catch (e) {
         expect(e.status).toBe(404);
@@ -93,6 +94,68 @@ module.exports = (v = ``) => {
         expect(e.status).toBe(400);
       }
 
+    }));
+
+    it(`delete`, testAsync(async function() {
+      const doc     = await upsert(coll, Object.assign({}, defaultData));
+      const { res } = await emit(`deleteLexeme`, doc.id);
+      expect(res.status).toBe(204);
+    }));
+
+    it(`get`, testAsync(async function() {
+      const doc = await upsert(coll, Object.assign({ tid: `get` }, defaultData));
+      const { res: lex } = await emit(`getLexeme`, doc.id);
+      expect(lex.tid).toBe(`get`);
+    }));
+
+    it(`getAll`, testAsync(async function() {
+
+      const data = {
+        languageID,
+        lemma: {},
+        permissions: { owner: [`some-other-user`] },
+        test,
+        tid: `getAll`,
+        type,
+      };
+
+      const doc = await upsert(coll, data);
+      await upsert(coll, Object.assign({}, defaultData));
+      const { res, info } = await emit(`getLexemes`);
+
+      expect(res.length).toBeGreaterThan(0);
+      expect(info.itemCount).toBeGreaterThan(0);
+      expect(res.some(item => item.tid === doc.tid)).toBe(false);
+
+    }));
+
+    it(`update`, testAsync(async function() {
+
+      const data = Object.assign({
+        notChanged: `This property should not be changed.`,
+        tid:        `upsertOne`,
+      }, defaultData);
+
+      const doc = await upsert(coll, data);
+
+      const newData = {
+        id: doc.id,
+        test,
+        tid: `upsertOneAgain`,
+        type,
+      };
+
+      const { res } = await emit(`updateLexeme`, newData);
+
+      expect(res.notChanged).toBe(doc.notChanged);
+      expect(res.tid).toBe(newData.tid);
+
+    }));
+
+    it(`upsert`, testAsync(async function() {
+      const data = Object.assign({ tid: `upsert` }, defaultData);
+      const { res } = await emit(`upsertLexeme`, data);
+      expect(res.tid).toBe(data.tid);
     }));
 
   });
